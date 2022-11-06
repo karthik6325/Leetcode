a2d04a1c7e0a149f8fa24b88aaf656aad021ca88
karthik6325 <60821631+karthik6325@users.noreply.github.com>
2022-11-07 00:19:17 +0530

    Time: 4 ms (98.26%), Space: 10.6 MB (65.65%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cpp
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cpp
@@ -1,22 +1,8 @@
 class Solution {
 public:
-    bool s(int b,int e,vector<int>& nums,int t){
-        while(b<=e){
-            int m=b+(e-b)/2;
-            if(t==nums[m])
-                return true;
-            else if(nums[m]>t)
-                e=m-1;
-            else
-                b=m+1;
-        }
-        return false;
-    }
     int findFinalValue(vector<int>& nums, int o) {
-        int b=0,e=nums.size()-1;
-        sort(nums.begin(),nums.end());
         while(1){
-            if(s(b,e,nums,o))
+            if(find(nums.begin(),nums.end(),o)!=nums.end())
                 o*=2;
             else break;
         }
