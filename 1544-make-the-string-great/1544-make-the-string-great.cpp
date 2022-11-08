7fcc7d6d8521279747d1d3367fe53cfae2f834d4
karthik6325 <60821631+karthik6325@users.noreply.github.com>
2022-11-08 22:14:27 +0530

    Time: 5 ms (54.29%), Space: 6.3 MB (82.16%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
-    string makeGood(string s) {
-        int t=s.size()-1;
-        for(int i=0;i<t;i++){
-            if(i<0)
-                i=0;
-            if(s[i]-'a'==s[i+1]-'A'||s[i]-'A'==s[i+1]-'a'){
-                s.erase(i,2);
-                i-=2;
-            }
-            t=s.size()-1;
-        }
-        return s;
+    string makeGood(string s, int p = 0) {
+    for (int i = 0; i < s.size(); ++i) {
+        if (p > 0 && abs(s[i] - s[p - 1]) == 32)
+            --p;
+        else
+            s[p++] = s[i];
     }
+    return s.substr(0, p);
+}
 };
