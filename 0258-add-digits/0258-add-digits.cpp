bf57f579fed253ddae185882c3d3c71a74b45196
karthik6325 <60821631+karthik6325@users.noreply.github.com>
2022-11-14 23:28:22 +0530

    Time: 5 ms (30.77%), Space: 6 MB (25.64%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,19 +1,6 @@
 class Solution {
 public:
-    int d(int n){
-        int d=0;
-        while(n>0){
-            d+=n%10;
-            n/=10;
-        }
-        return d;
-    }
     int addDigits(int n) {
-        int ans=n;
-        while(n>10){
-            ans=d(n);
-            n=ans;
-        }
-        return ans>9?d(ans):ans;
+        return 1+(n-1)%9;
     }
 };
