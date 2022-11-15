7194f9ddb5ad90ee93ba11a3aa80fa0de9054530
karthik6325 <60821631+karthik6325@users.noreply.github.com>
2022-11-15 14:56:38 +0530

    Time: 0 ms (100.00%), Space: 6 MB (27.98%) - LeetHub

---
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minMoves(int t, int m) {
+        int ans=0;
+        while(t>0){
+            if(t/2>1&&m){
+                if(t%2==0){
+                    t/=2;
+                    m--;
+                }
+                else
+                    t--;
+                ans++;
+            }
+            else{
+                if(t==1)
+                    break;
+                ans+=t-1;
+                break;
+            }
+        }
+        return ans;
+    }
+};
