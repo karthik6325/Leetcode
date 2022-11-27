4c0a586bbe527ce4e7ef22a1fdb97058cd028c6c
karthik6325 <60821631+karthik6325@users.noreply.github.com>
2022-11-27 20:33:21 +0530

    Time: 499 ms (13.01%), Space: 88.2 MB (51.53%) - LeetHub

--- 2344-minimum-deletions-to-make-array-divisible/2344-minimum-deletions-to-make-array-divisible.cpp
+++ 2344-minimum-deletions-to-make-array-divisible/2344-minimum-deletions-to-make-array-divisible.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, vector<int>& numsd) {
-        priority_queue<int,vector<int>,greater<int>>p;
         int g=0;
         for(int i=0;i<numsd.size();i++)
             g=__gcd(g,numsd[i]);
-        for(int i=0;i<nums.size();i++)
-            p.push(nums[i]);
+        sort(nums.begin(),nums.end());
         int c=0;
         for(int i=0;i<nums.size();i++){
-            if(g%p.top()==0){
+            if(g%nums[i]==0)
                 return c;
-            }
-            p.pop();
             c++;
         }
         return -1;
