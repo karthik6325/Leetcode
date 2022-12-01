704404fe858d9b127ff09b3e7e61f3bfa9a5d74d
karthik6325 <60821631+karthik6325@users.noreply.github.com>
2022-12-01 23:49:33 +0530

    Time: 2 ms (89.18%), Space: 6.4 MB (99.76%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -2,10 +2,12 @@ class Solution {
 public:
     bool halvesAreAlike(string s) {
         int n=s.size(),c1=0,c2=0;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n/2;i++){
             if(i<n/2&&(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'))
                 c1++;
-            else if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U')
+        }
+        for(int i=n/2;i<n;i++){
+            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U')
                 c2++;
             
         }
