c4ffb33817442df0d93e10a7dfac715345592d76
karthik6325 <60821631+karthik6325@users.noreply.github.com>
2022-12-03 22:41:06 +0530

    Time: 36 ms (52.14%), Space: 19.3 MB (42.79%) - LeetHub

--- 0365-water-and-jug-problem/0365-water-and-jug-problem.cpp
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     bool canMeasureWater(int a, int b, int c) {
-        return c==0||c<=a+b&&c%__gcd(a,b)==0;
+        int possible[]={a,-a,b,-b};
+        queue<int>q;
+        q.push(0);
+        vector<int>vis(a+b+1,0);
+        vis[0]=1;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            if(node==c)
+                return true;
+            for(int i=0;i<4;i++){
+                int t=node+possible[i];
+                if(t>=0&&t<=a+b&&vis[t]==0){
+                    q.push(t);
+                    vis[t]=1;
+                }
+            }
+        }
+        return false;
     }
 };
