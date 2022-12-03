2921d06a9144246587fe573747f841b30f7b3973
karthik6325 <60821631+karthik6325@users.noreply.github.com>
2022-12-03 22:20:25 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (84.46%) - LeetHub

--- 0365-water-and-jug-problem/0365-water-and-jug-problem.cpp
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     bool canMeasureWater(int a, int b, int c) {
-        if(c>a+b)
-            return 0;
-        return c==0||c%__gcd(a,b)==0;
+        return c==0||c<=a+b&&c%__gcd(a,b)==0;
     }
 };
